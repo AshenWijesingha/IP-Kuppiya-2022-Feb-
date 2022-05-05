27a210157a2712d6212c2b0e7d342dd5d57ae9d9
Ashen_Wijesingha <66056859+AshenWijesingha@users.noreply.github.com>
2022-05-05 10:57:28 +0000

    Added Array Content

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+# include <string.h>
+# define SIZE1 25
+# define SIZE2 15
 
-int main(void) {
-  printf("Hello World\n");
-  return 0;
-}
+int main ( void )
+{
+  
+char x[ ]= "Happy Birthday to You";
+char y[ SIZE1];
+strcpy( y , x );
+printf("The string in array y is : %s\n", y);
+  
+return 0;
+  
+} 
