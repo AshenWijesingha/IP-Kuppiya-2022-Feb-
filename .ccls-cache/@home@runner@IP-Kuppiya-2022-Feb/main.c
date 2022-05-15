f973a00a4ec9fcf4b6941c6472ef61cebc0ff85b
Ashen_Wijesingha <66056859+AshenWijesingha@users.noreply.github.com>
2022-05-15 23:28:51 +0000

    Added Example For Three Dimensional Array

--- .ccls-cache/@home@runner@IP-Kuppiya-2022-Feb/main.c
+++ .ccls-cache/@home@runner@IP-Kuppiya-2022-Feb/main.c
@@ -5,11 +5,58 @@
 
 int main ( void )
 {
+
+float num[2][4][3];
+
+ /*
+Three Dimensional Array
+
+{
+  {//2
+    {4,4,4,4}, //3
+    {4,4,4,4}, //3
+    {4,4,4,4}  //3
+  },
+
+  {//2
+      {4,4,4,4}, //3
+      {4,4,4,4}, //3
+      {4,4,4,4}  //3
+  }
+};
+
+
+*/
+
+//store 12 values entered by the user to the Three Diamentional Array
   
-char x[ ]= "Happy Birthday to You";
-char y[ SIZE1];
-strcpy( y , x );
-printf("The string in array y is : %s\n", y);
+printf("Enter 12 values: \n");
+
+  for (int i = 0; i < 2; ++i)
+  {
+    for (int j = 0; j < 3; ++j)
+    {
+      for (int k = 0; k < 4; ++k)
+      {
+        scanf("%f", &num[i][j][k]);
+      }
+    }
+  }
+
+// Printing values with the proper index.
+
+  printf("\nDisplaying values:\n");
+  for (int i = 0; i < 2; ++i)
+  {
+    for (int j = 0; j < 3; ++j)
+    {
+      for (int k = 0; k < 4; ++k)
+      {
+        printf("test[%d][%d][%d] = %f\n", i, j, k, num[i][j][k]);
+      }
+    }
+  }
+
   
 return 0;
   
