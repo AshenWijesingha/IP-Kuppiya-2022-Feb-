2c0397e12cd03206aa647fe8b9e47595e10d8b8a
Ashen_Wijesingha <66056859+AshenWijesingha@users.noreply.github.com>
2022-05-15 23:41:56 +0000

    Add explanation for 1D Array

--- main.c
+++ main.c
@@ -5,11 +5,41 @@
 
 int main ( void )
 {
+
+float num[5];
+
+/*
+One Diamentional Array
+
+dataType arrayName[arraySize];
+
+num{
+
+[1]
+[2]
+[3]
+[4]
+[5]
+
+}
+
+Ex: - int mark[5] = {19, 10, 8, 17, 9};
+
+*/
+
+  // taking input and storing it in an array
+  printf("Enter 5 Inputs To The Array: ");
   
-char x[ ]= "Happy Birthday to You";
-char y[ SIZE1];
-strcpy( y , x );
-printf("The string in array y is : %s\n", y);
+  for(int i = 0; i < 5; ++i) {
+     scanf("%f", &num[i]);
+  }
+
+  // printing elements of an array
+  printf("Print Stored Data in Array: ");
+  
+  for(int i = 0; i < 5; ++i) {
+     printf("%f\n", num[i]);
+  }
   
 return 0;
   
