1a7d30383a40b2d8f4d96ec19a059781b3d8a841
Ashen_Wijesingha <66056859+AshenWijesingha@users.noreply.github.com>
2022-05-15 20:38:19 +0000

    Added Few Code Lines

--- .ccls-cache/@home@runner@IP-Kuppiya-2022-Feb/main.c
+++ .ccls-cache/@home@runner@IP-Kuppiya-2022-Feb/main.c
@@ -1,8 +1,56 @@
-#include <stdio.h>
+# include <stdio.h>
 
-int main(void) {
+int main(void){
+
+  int counter = 1;
+  int choice = 1;
+  int val = 0;
+
+    //sentinal control repetition
+    while(choice == -1){ // check for the value
+      //statements 
+    }
+
+    //counter control repetition
+    if(counter <= 10){
+      if(choice == -1){
+        val = 1; 
+      }
+    }
+/*  while( choice != -1 )
+      {
+        if( counter > 10 )
+
+          printf("%d \n", counter);
+          ++counter;
+          choice = -1;
+        
+      }
   
-  printf("Hello World\n");
+    // while (val == 1){ //check for the count  
+
+    // printf("%d \n", counter);
+    // ++counter;
+    // choice = 1;
+
+    // if(counter <= 10){
+    //   if(choice == -1){
+    //     val = 1; 
+    //   }
+    // }  
+      
+    // }
+
+    // printf("End of while loop");
+  
+    // do{
+      
+    //     printf("%d \n", counter);
+    //     ++counter;
+      
+    //   } while (counter <= 10);
+
+    // printf("End of Do-while loop");
   
   return 0;
-}
+} 
