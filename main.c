df6aa2f1388d0ded01b22480031022a86e902898
Ashen_Wijesingha <66056859+AshenWijesingha@users.noreply.github.com>
2022-05-16 00:05:45 +0000

    Counter Contro Repetetion

--- main.c
+++ main.c
@@ -2,7 +2,20 @@
 
 int main(void) {
   
-  printf("Hello World\n");
+/*
+
+while (testExpression) {
+  // the body of the loop 
+}
+
+*/
+ 
+  int i = 1;
+    
+  while (i <= 5) {
+    printf("%d\n", i);
+    ++i;
+  }
   
   return 0;
 }
