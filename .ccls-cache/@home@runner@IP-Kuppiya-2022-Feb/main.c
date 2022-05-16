62eee61a8d24539e37ad72def98f9f32acb38619
Ashen_Wijesingha <66056859+AshenWijesingha@users.noreply.github.com>
2022-05-16 09:32:53 +0000

    Added Few Changes

--- .ccls-cache/@home@runner@IP-Kuppiya-2022-Feb/main.c
+++ .ccls-cache/@home@runner@IP-Kuppiya-2022-Feb/main.c
@@ -4,22 +4,62 @@ int main(void) {
 
   
 /*
+//Type 1
 
 for (initializationStatement; testExpression; updateStatement)
 {
     // statements inside the body of loop
 }
 
+##############################
+
+//Type 2
+
+initializationStatement
+
+for ( ; testExpression; updateStatement)
+{
+    // statements inside the body of loop
+}
+
+##############################
+
+//type 3
+
+initializationStatement
+
+for ( ; testExpression; )
+{
+    // statements inside the body of loop
+    updateStatement; //++, --, 
+}
+
 */
 
   // Print numbers from 1 to 10
-  int i;
+  
 
-  for (i = 1; i < 11; ++i)
+  for (int i = 1; i < 11; ++i) // type 1
   {
     printf("%d ", i);
   }
-
+/*#######################################*/
+  int j = 1;
+  
+  for (; j < 11; ++j) // type 2
+  {
+    printf("%d ", j);
+  }
+/*#######################################*/
+  int k = 1;
+  
+  for (; k < 11;) // type 3
+  {
+    printf("%d ", k);
+    ++k;
+  }
+  
+  
 /*
 -Example Question-
 
@@ -37,8 +77,6 @@ int num, count, sum = 0;
     printf("Sum = %d", sum);
 
 */
-
-
   
   return 0;
 }
