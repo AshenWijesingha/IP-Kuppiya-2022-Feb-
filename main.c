1c6471ea8960e6208d7d88fd8a7e5aa62680f4c8
Ashen_Wijesingha <66056859+AshenWijesingha@users.noreply.github.com>
2022-05-16 11:23:31 +0000

    Added few code segmants

--- main.c
+++ main.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 # include <string.h>
-# define SIZE1 25
-# define SIZE2 15
 
 int main ( void )
 {
 
 float num[5];
+
 
 /*
 One Diamentional Array
@@ -15,30 +14,33 @@ dataType arrayName[arraySize];
 
 num{
 
-[1]
-[2]
-[3]
-[4]
-[5]
+[1]-0
+[2]-1
+[3]-2
+[4]-3
+[5]-4
 
 }
-
+                      0   1  2  3   4 
 Ex: - int mark[5] = {19, 10, 8, 17, 9};
+
+mark[2] == 8
+mark[3] == 17
 
 */
 
   // taking input and storing it in an array
-  printf("Enter 5 Inputs To The Array: ");
+  printf("Enter 5 Inputs To The Array: \n");
   
   for(int i = 0; i < 5; ++i) {
      scanf("%f", &num[i]);
   }
 
   // printing elements of an array
-  printf("Print Stored Data in Array: ");
+  printf("Print Stored Data in Array: \n");
   
   for(int i = 0; i < 5; ++i) {
-     printf("%f\n", num[i]);
+     printf("num[ %d ] - %.1f\n",i, num[i]);
   }
   
 return 0;
