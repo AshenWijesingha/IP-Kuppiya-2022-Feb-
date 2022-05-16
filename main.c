02ee9f2d6ad846233c4baab6a1f00b1be58f393d
Ashen_Wijesingha <66056859+AshenWijesingha@users.noreply.github.com>
2022-05-16 00:12:23 +0000

    For Loop

--- main.c
+++ main.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 
 int main(void) {
+
   
-  printf("Hello World\n");
+/*
+
+for (initializationStatement; testExpression; updateStatement)
+{
+    // statements inside the body of loop
+}
+
+*/
+
+  // Print numbers from 1 to 10
+  int i;
+
+  for (i = 1; i < 11; ++i)
+  {
+    printf("%d ", i);
+  }
   
   return 0;
 }
