2940e6301cff8e0430e35e13bf240b76eebc410b
Ashen_Wijesingha <66056859+AshenWijesingha@users.noreply.github.com>
2022-05-16 00:13:29 +0000

    For Loop Question Added

--- .ccls-cache/@home@runner@IP-Kuppiya-2022-Feb/main.c
+++ .ccls-cache/@home@runner@IP-Kuppiya-2022-Feb/main.c
@@ -19,6 +19,26 @@ for (initializationStatement; testExpression; updateStatement)
   {
     printf("%d ", i);
   }
+
+/*
+-Example Question-
+
+int num, count, sum = 0;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+
+    // for loop terminates when num is less than count
+    for(count = 1; count <= num; ++count)
+    {
+        sum += count;
+    }
+
+    printf("Sum = %d", sum);
+
+*/
+
+
   
   return 0;
 }
